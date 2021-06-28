ab1489ac6dcfe70da07ccd923924825b48968a06
wkjung0624 <>
2021-06-28 17:36:34 +0900

    Add javascript practice

---
+++ 210628/user_data.js
@@ -0,0 +1,121 @@
+var userList = [{
+        "_id": "60646eed43013f9997469747",
+        "isActive": false,
+        "balance": "$2,534.72",
+        "picture": "http://placehold.it/32x32",
+        "age": 25,
+        "eyeColor": "green",
+        "name": "Henrietta Dickerson",
+        "gender": "female"
+    },
+    {
+        "_id": "60646eed345806ed53897ce9",
+        "isActive": false,
+        "balance": "$2,333.05",
+        "picture": "http://placehold.it/32x32",
+        "age": 24,
+        "eyeColor": "brown",
+        "name": "Medina Hopper",
+        "gender": "male"
+    },
+    {
+        "_id": "60646eed4caf6f5673b47536",
+        "isActive": true,
+        "balance": "$3,091.32",
+        "picture": "http://placehold.it/32x32",
+        "age": 26,
+        "eyeColor": "blue",
+        "name": "Danielle Berry",
+        "gender": "female"
+    },
+    {
+        "_id": "60646eed80d49669489807ca",
+        "isActive": false,
+        "balance": "$1,344.95",
+        "picture": "http://placehold.it/32x32",
+        "age": 36,
+        "eyeColor": "brown",
+        "name": "Aguilar Carpenter",
+        "gender": "male"
+    },
+    {
+        "_id": "60646eed76d5ddfa333f8ede",
+        "isActive": false,
+        "balance": "$3,686.58",
+        "picture": "http://placehold.it/32x32",
+        "age": 32,
+        "eyeColor": "green",
+        "name": "Carney Burns",
+        "gender": "male"
+    },
+    {
+        "_id": "60646eed63a3bcdbf0c9b3bd",
+        "isActive": false,
+        "balance": "$2,430.65",
+        "picture": "http://placehold.it/32x32",
+        "age": 33,
+        "eyeColor": "brown",
+        "name": "Nikki Craig",
+        "gender": "female"
+    },
+    {
+        "_id": "60646eed8fcaace8d93863a1",
+        "isActive": false,
+        "balance": "$2,415.18",
+        "picture": "http://placehold.it/32x32",
+        "age": 36,
+        "eyeColor": "blue",
+        "name": "Velma Beck",
+        "gender": "female"
+    },
+    {
+        "_id": "60646eed201c5be13d9093ca",
+        "isActive": true,
+        "balance": "$1,845.59",
+        "picture": "http://placehold.it/32x32",
+        "age": 34,
+        "eyeColor": "brown",
+        "name": "Charity Hess",
+        "gender": "female"
+    },
+    {
+        "_id": "60646eed31ab2cc97ccb82ee",
+        "isActive": false,
+        "balance": "$1,569.87",
+        "picture": "http://placehold.it/32x32",
+        "age": 40,
+        "eyeColor": "brown",
+        "name": "Mcclure Sears",
+        "gender": "male"
+    },
+    {
+        "_id": "60646eed05078e67093983bb",
+        "isActive": false,
+        "balance": "$1,278.78",
+        "picture": "http://placehold.it/32x32",
+        "age": 30,
+        "eyeColor": "blue",
+        "name": "Davis Juarez",
+        "gender": "male"
+    },
+    {
+        "_id": "60646eed2617be2851744528",
+        "isActive": true,
+        "balance": "$2,149.30",
+        "picture": "http://placehold.it/32x32",
+        "age": 21,
+        "eyeColor": "green",
+        "name": "Wallace Pollard",
+        "gender": "male"
+    },
+    {
+        "_id": "60646eede896f5bb2762dbac",
+        "isActive": false,
+        "balance": "$2,053.16",
+        "picture": "http://placehold.it/32x32",
+        "age": 34,
+        "eyeColor": "brown",
+        "name": "Jo Woodward",
+        "gender": "female"
+    }
+];
